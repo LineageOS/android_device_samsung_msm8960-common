39ffb55e35c76b5c9271db59db9ffc39c3a60e41
Steven <stevenharperuk.aosp@gmail.com>
2012-08-02 19:22:38 +0100

    Add Dock USB Audio Support (3/3)
    Kanged from StephenHarperUK/i9100 by Araemo
    
    This adds a new Tab in the GalaxyS3Settings.apk called Dock
    
    There is a new section in this for Dock Audio.
    A new Checkbox sets the users preference to route the Dock Audio
    
    On Startup or when the checkbox is changed, an intent is fired
    
    This intent is recieved by the services/java/com/android/server/WiredAccessoryObserver.java
    See: http://review.cyanogenmod.com/#/c/20586/
    
    On d2, the redirection does nothing without an audio_policy.conf and alsa config change - ( 2/3: http://review.cyanogenmod.com/#/c/25278/ )
    
    Also requires an alsa change in hardware/qcom/audio (1/3: http://review.cyanogenmod.com/#/c/25279/ )
    
    Conflicts:
    
    	DeviceSettings/res/values/strings.xml
    	DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
    	DeviceSettings/src/com/cyanogenmod/settings/device/Startup.java
    
    Change-Id: I86579c8e6c031d9251d24a959c4198f333dbfe5b

--- DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
+++ DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
@@ -51,6 +51,7 @@ public class DeviceSettings extends FragmentActivity {
     public static final String CATEGORY_SENSORS = "sensors";
     public static final String CATEGORY_TOUCHSCREEN = "touchscreen";
     public static final String CATEGORY_TOUCHKEY = "touchkey";
+    public static final String KEY_USE_DOCK_AUDIO = "dock_audio";
 
     ViewPager mViewPager;
     TabsAdapter mTabsAdapter;
@@ -73,6 +74,8 @@ public class DeviceSettings extends FragmentActivity {
                 mDNIeFragmentActivity.class, null);
         mTabsAdapter.addTab(bar.newTab().setText(R.string.category_sensors_title),
                 SensorsFragmentActivity.class, null);
+        mTabsAdapter.addTab(bar.newTab().setText(R.string.category_dock_title),
+                DockFragmentActivity.class, null);
 
         if (savedInstanceState != null) {
             bar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));
