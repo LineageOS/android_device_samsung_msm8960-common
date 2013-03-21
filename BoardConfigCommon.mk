ea5c5187c24843b98127d9ecf644edd6bb4758e6
Shareef Ali <shareefalis@cyanogenmod.org>
2013-03-21 13:37:01 -0700

    Revert "msm8960: temp disable KERNEL_HAS_GETTIMEOFDAY_HELPER;"
    This reverts commit a9f44cd5597da078d76f2eec5784717e7ad69e73
    
    Change-Id: I3b14e12e77b6a645eb763d2e7e5e020a14c3c716

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -91,8 +91,7 @@ BOARD_NEEDS_MEMORYHEAPPMEM := true
 BOARD_HAVE_OLD_ION_API := true
 
 # Kernel time optimization
-# temp remove - causing issues with short/long presses
-# KERNEL_HAS_GETTIMEOFDAY_HELPER := true
+KERNEL_HAS_GETTIMEOFDAY_HELPER := true
 
 # We have the new GPS driver
 BOARD_HAVE_NEW_QC_GPS := true
