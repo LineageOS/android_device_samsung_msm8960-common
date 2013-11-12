11179582e09df46edecb612630d30e920df5073c
Matt Filetto <matt.filetto@gmail.com>
2013-11-12 01:18:19 -0800

    msm8960-common: remove build optimizations to fix compile.
    Not sure if this is still needed. back on saturday it was.
    
    Change-Id: I843355de28971e0081f97385cfde6e86f356daee

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,8 +29,6 @@ COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
 ifneq ($(VARIENT_REQUIRE_3.0_KERNEL),true)
 COMMON_GLOBAL_CFLAGS += -DNEW_ION_API
 endif
-TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_CPU_VARIANT := krait
 
 # Adreno configuration
