00abb261a8a501fe508e205eae85dcb632096ce4
Dave Daynard <nardholio@gmail.com>
2013-12-11 20:03:45 -0500

    msm8960-common: let's try not to clear the flags before they're used
    Change-Id: Iefba28f097eab5936cb7294bc3efd0e6c8f6a505

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,12 +1,12 @@
 # Currently only for apexqtmo and expressatt
 ifeq ($(TARGET_PROVIDES_CAMERA_HAL),true)
 
-ifneq ($(TARGET_PRODUCT),$(filter $(TARGET_PRODUCT),cm_apexqtmo cm_expressatt))
-    LOCAL_CFLAGS += -DENABLE_ZSL
-endif
-
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
+
+ifeq ($(filter cm_apexqtmo cm_expressatt,$(TARGET_PRODUCT)),)
+    LOCAL_CFLAGS += -DENABLE_ZSL
+endif
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
