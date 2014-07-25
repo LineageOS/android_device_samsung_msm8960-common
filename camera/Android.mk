debee523561566f2b7f7e7f6fa251525a735b27d
Ethan Chen <intervigil@gmail.com>
2014-07-25 10:39:26 -0700

    msm8960-common: Clean up Samsung device inheritance
    * Separate jflte from msm8960-common
    * Move msm8960 HALs to msm8960-common
    * Create firmware symlinks at compile time
    * Remove unused keylayout files
    * Split proprietary lib handling between msm8960-common and device trees
    * Move postrecoveryboot script up to qcom-common
    * Remove init.qcom.class_main and init.qcom.class_core
    * Remove unused qmiproxy
    * Remove init.qcom.early_boot
    * Remove init.qcom.syspart_fixup
    * Move thermald.conf to msm8960-common
    * Remove init.qcom.sh
    * Remove init.qcom.post_boot.sh
    * Remove init.qcom.usb.sh
    * Merge init.target.rc into init.qcom.rc
    * Re-organize and clean init.qcom.rc
    * Move apexq/express specific init bits to device init.target.rc
    * Move QCOM_BSP to platform repos
    
    Change-Id: Iaf9386fa647aa0c0d111c17c191317fe04d85fdd

--- camera/Android.mk
+++ camera/Android.mk
@@ -57,7 +57,7 @@ LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE := camera.MSM8960
+LOCAL_MODULE := camera.msm8960
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
