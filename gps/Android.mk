93f65f2f168f14af69c1a8783a981de47fcd2ebb
jt1134 <jt1134@gmail.com>
2014-09-09 11:08:09 -0700

    msm8960-common: allow device to provide its own GPS HAL
    - expressatt is using older JB RIL/GPS blobs, and cannot use the
      updated GPS HAL from msm8960-common.
    
    Change-Id: I9c8d0fca7144cf326774b0287d78095eab11790e

--- gps/Android.mk
+++ gps/Android.mk
@@ -22,10 +22,11 @@
 
 LOCAL_PATH := $(call my-dir)
 
-
 ifeq ($(BOARD_VENDOR),samsung)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
+ifneq ($(TARGET_PROVIDES_GPS_HAL),true)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
 endif
 endif
 
