869cdaa5d3b791edc802ffeb956a34c30bf39156
Ethan Chen <intervigil@gmail.com>
2014-11-16 22:59:18 -0700

    Add qcmediaplayer
    Change-Id: I43b3aa2187b189458c6df25eb45675bab491bde2

--- msm8960.mk
+++ msm8960.mk
@@ -164,7 +164,11 @@ PRODUCT_PACKAGES += \
     libOmxAmrEnc \
     libOmxEvrcEnc \
     libOmxQcelp13Enc \
-    libstagefrighthw
+    libstagefrighthw \
+    qcmediaplayer
+
+PRODUCT_BOOT_JARS += \
+    qcmediaplayer
 
 # Power
 PRODUCT_PACKAGES += \
